9088bf8ee69fc15de96f01e3812da5cd98a23d94
Styko <smisobb@gmail.com>
2018-10-16 09:58:49 +0200

    checking git

--- src/main/java/tacos/model/Order.java
+++ src/main/java/tacos/model/Order.java
@@ -23,7 +23,7 @@ import lombok.Data;
 @Entity
 @Table(name = "Taco_Order")
 public class Order {
-	private static final long serialVersionUID = 1L;
+	//private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
